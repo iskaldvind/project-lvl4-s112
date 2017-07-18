4b624abb6ef611746bcd28096a60a8c037c808b9
iskaldvind <iskaldvind@gmail.com>
2017-07-18 16:15:58 +0300

    and again

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,7 +4,7 @@ import webpack from 'webpack';
 export default () => ({
   entry: {
     app: ['./client'],
-    vendor: ['babel-polyfill', 'jquery', 'jquery-ujs', 'autoprefixer'],
+    vendor: ['babel-polyfill', 'jquery', 'jquery-ujs', 'autoprefixer', 'postcss'],
   },
   output: {
     path: path.join(__dirname, 'public', 'assets'),
