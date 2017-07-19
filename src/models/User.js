29c66feec75387d6dcb3da4d4a04342710a6890d
iskaldvind <iskaldvind@gmail.com>
2017-07-19 12:14:53 +0300

    and again

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,7 @@
 import Sequelize from 'sequelize';
 import encrypt from '../helpers/secure';
 
-export default connect => connect.define('user', {
+export default connect => connect.define('User', {
   email: {
     type: Sequelize.STRING,
     unique: true,
