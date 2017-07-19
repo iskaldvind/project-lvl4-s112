1db185456625fd21b12b0bbf263251d8940f104b
iskaldvind <iskaldvind@gmail.com>
2017-07-19 12:43:41 +0300

    and again

--- src/db/index.js
+++ src/db/index.js
@@ -2,5 +2,4 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const pgUri = 'postgres://ruxycfchvyavxx:81e4ec2c1fe618f31b69f05e5abe6cc40607897c2e1038b9353035375365123c@ec2-54-225-71-119.compute-1.amazonaws.com:5432/den7qhvrnm0ibd';
-export default new Sequelize(process.env.DATABASE_URL || pgUri);
+export default new Sequelize(process.env.DATABASE_URL);
