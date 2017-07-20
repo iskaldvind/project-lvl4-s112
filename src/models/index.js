5c4711821e1755cc75e348bb7502b8b8e14a55e9
iskaldvind <iskaldvind@gmail.com>
2017-07-20 19:23:33 +0300

    and again

--- src/models/index.js
+++ src/models/index.js
@@ -3,9 +3,20 @@ import getTag from './Tag';
 import getTask from './Task';
 import getTaskStatus from './TaskStatus';
 
-export default connect => ({
-  User: getUser(connect),
-  Tag: getTag(connect),
-  Task: getTask(connect),
-  TaskStatus: getTaskStatus(connect),
-});
+export default connect => {
+  const models = {
+    User: getUser(connect),
+    Tag: getTag(connect),
+    Task: getTask(connect),
+    TaskStatus: getTaskStatus(connect),
+  };
+
+  User.hasMany(Task);
+  Task.belongsTo(User);
+  Task.hasMany(Tag);
+  Tsg.belongsToMany(Task);
+  Task.hasOne(TaskStatus);
+  TaskStatus.belongsToMany(Task);
+  
+  return models;
+};
