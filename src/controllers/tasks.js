b17bcfbdc397cb001b0c7e6987d45b1d73e189e5
iskaldvind <iskaldvind@gmail.com>
2017-07-21 15:34:32 +0300

    and again

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -2,10 +2,11 @@ import buildFormObj from '../helpers/formObjectBuilder';
 
 export default (router, { User }) => {
   router
-    .get('users_list', '/users', async (ctx) => {
-      const users = await User.findAll();
-      ctx.render('users', { users });
+    .get('tasks_list', '/tasks', async (ctx) => {
+      const tasks = await Task.findAll();
+      ctx.render('tasks', { tasks });
     })
+    /*
     .get('user_reg', '/users/new', async (ctx) => {
       const user = User.build();
       ctx.render('users/new', { f: buildFormObj(user) });
@@ -64,4 +65,5 @@ export default (router, { User }) => {
         ctx.redirect(router.url('root'));
       }
     });
+    */
 };
