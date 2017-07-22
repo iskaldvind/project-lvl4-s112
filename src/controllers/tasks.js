f088734187cc575a8d3f36db13bf9cf4eb5062de
iskaldvind <iskaldvind@gmail.com>
2017-07-22 22:15:04 +0300

    good commit

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -28,11 +28,8 @@ export default (router, { Task, User, Tag, TaskStatus }) => {
       const form = ctx.request.body.form;
       form.creatorId = ctx.state.signedId();
       const users = await User.findAll();
-      console.log(form);
-      const filledForm = form.tags !== '' ? form : { ...form, tags: '    '};
-      console.log(filledForm);
+      const filledForm = form.tags !== '' ? form : { ...form, tags: '-'};
       const tags = filledForm.tags.split(' ');
-      console.log(tags);
       const task = Task.build(filledForm);
       try {
         await task.save();
