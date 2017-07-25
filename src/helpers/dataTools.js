09b7751c4778c60e94f456b1f65528e13fd9939d
iskaldvind <iskaldvind@gmail.com>
2017-07-25 19:11:24 +0300

    catching bugs

--- src/helpers/dataTools.js
+++ src/helpers/dataTools.js
@@ -86,10 +86,10 @@ export const updateTags = async (tags, Tag, task) => {
   console.log(tags);
   console.log('IIIIIIIIIIIII task:');
   console.log(task);
-  // const currentTaskTags = await task.tags;
+  const currentTaskTags = await task.getTags();
   // const deletedTags = await currentTaskTags.filter(currentTag => tags.indexOf(currentTag) === 1);
   console.log('KKKKKKKKKKKKKKKKKKk deleted tags:');
-  // console.log(deletedTags);
+  console.log(currentTaskTags);
   await tags.map(tag => Tag.findOne({ where: { name: tag } })
     .then(async result => (result ? task.addTag(result) :
       task.createTag({ name: tag }))));
