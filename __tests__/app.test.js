830bc46c7186d2aedcccefdfa29e88222eb5af49
iskaldvind <iskaldvind@gmail.com>
2017-08-12 12:10:41 +0300

    make things even better

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -100,43 +100,103 @@ describe('Authentication', () => {
   });
 });
 
-describe('Get data', () => {
-  const { email, firstName, lastName, password, userAgent } = fake();
+describe('Self profile manipulations', () => {
+  const user3 = fake();
+  const user3Edited = fake();
+  const user4 = fake();
   const server = app().listen();
-  let cookie;
   const superagent = request.agent(server);
 
-  it('Get user', async () => {
+  beforeAll(async () => {
     await superagent
       .post('/users')
       .type('form')
-      .send({ form: { email, firstName, lastName, password } })
-      .set('user-agent', userAgent)
+      .send({ form:
+        {
+          email: user3.email,
+          firstName: user3.firstName,
+          lastName: user3.lastName,
+          password: user3.password,
+        },
+      })
+      .set('user-agent', user3.userAgent)
       .set('Connection', 'keep-alive')
       .set('content-type', 'application/x-www-form-urlencoded')
+      .set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
+    await superagent
+      .post('/users')
+      .type('form')
+      .send({ form:
+        {
+          email: user4.email,
+          firstName: user4.firstName,
+          lastName: user4.lastName,
+          password: user4.password,
+        },
+      })
+      .set('user-agent', user4.userAgent)
+      .set('Connection', 'keep-alive')
+      .set('content-type', 'application/x-www-form-urlencoded')
+      .set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8');
+  });
+
+  it('Read profile', async () => {
+    await superagent
+      .get('/users/3')
+      .set('user-agent', user3.userAgent)
+      .set('x-test-auth-token', user3.email)
+      .set('Connection', 'keep-alive')
       .set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-      .then(async (res) => {
-        cookie = res.headers['set-cookie'];
+      .expect(200)
+      .expect(res => res.text.includes(user3.firstName));
+  });
+
+  it('Edit self profile', async () => {
+    await superagent
+      .patch('/users/3')
+      .type('form')
+      .send({ form:
+        {
+          email: user3.email,
+          firstName: user3Edited.firstName,
+          lastName: user3.lastName,
+          password: user3.password,
+        },
+      })
+      .set('user-agent', user3.userAgent)
+      .set('x-test-auth-token', user3.email)
+      .set('Connection', 'keep-alive')
+      .set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+      .expect(302)
+      .then(async () => {
         await superagent
-          .post('/sessions')
-          .type('form')
-          .send({ form: { email, password } })
-          .set('user-agent', userAgent)
-          .set('x-test-auth-token', email)
+          .get('/users/1')
+          .set('user-agent', user3.userAgent)
+          .set('x-test-auth-token', user3.email)
           .set('Connection', 'keep-alive')
-          .set('content-type', 'application/x-www-form-urlencoded')
-          .set('cookie', cookie)
           .set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-          .then(async () => {
-            await superagent
-              .get('/users/1')
-              .set('user-agent', userAgent)
-              .set('x-test-auth-token', email)
-              .set('Connection', 'keep-alive')
-              .set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-              .set('cookie', cookie)
-              .expect(200);
-          });
+          .expect(200)
+          .expect(res => res.text.includes(user3Edited.firstName));
+      });
+  });
+
+  it('Delete self profile', async () => {
+    await superagent
+      .delete('/users/3')
+      .set('user-agent', user3.userAgent)
+      .set('x-test-auth-token', user3.email)
+      .set('Connection', 'keep-alive')
+      .set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+      .expect(302)
+      .then(async () => {
+        await superagent
+          .get('/users')
+          .set('user-agent', user4.userAgent)
+          .set('x-test-auth-token', user4.email)
+          .set('Connection', 'keep-alive')
+          .set('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
+          .expect(200)
+          .expect(res => !res.text.includes(user3Edited.firstName));
       });
   });
 
